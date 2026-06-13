2f493fe1d9e392ef7fffe1a3474f38f31c5f5e8e
kalish1213-eng <kalish1213@gmail.com>
2026-06-14 02:42:18 +0300

    Replace branded 2A clue images

--- EF/src/state/useAnswersState.js
+++ EF/src/state/useAnswersState.js
@@ -1,4 +1,4 @@
-import { loadJson, saveJson } from "../utils/storage.js?v=20260614-progress1";
+import { loadJson, saveJson } from "../utils/storage.js?v=20260614-neutralbrand1";
 
 export function createAnswersState(storageKey, fallback = {}) {
   let value = loadJson(storageKey, fallback);
