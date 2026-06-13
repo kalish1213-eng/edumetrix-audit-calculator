abce168ebd6860a398a4027644bfff6d41c0abe1
kalish1213-eng <kalish1213@gmail.com>
2026-06-13 17:28:38 +0300

    Upgrade Revise and Check 7-8 imagery to lightweight WebP

--- EF/app.js
+++ EF/app.js
@@ -247,7 +247,7 @@ const nativeLessons = [
     startPage: 53,
     endPage: 54,
     title: "Revise and Check 7&8",
-    url: "public/native/revise-check-7-8/index.html?v=20260613-revise78photo1c"
+    url: "public/native/revise-check-7-8/index.html?v=20260613-revise78photo2"
   },
   {
     startPage: 55,
