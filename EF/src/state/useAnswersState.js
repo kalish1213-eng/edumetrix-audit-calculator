e742542b53053e2ccff699487449594a635fae56
kalish1213-eng <kalish1213@gmail.com>
2026-06-14 01:47:48 +0300

    Fix EF field overlap layouts

--- EF/src/state/useAnswersState.js
+++ EF/src/state/useAnswersState.js
@@ -1,4 +1,4 @@
-import { loadJson, saveJson } from "../utils/storage.js?v=20260614-overlapfix1";
+import { loadJson, saveJson } from "../utils/storage.js?v=20260614-overlapaudit2";
 
 export function createAnswersState(storageKey, fallback = {}) {
   let value = loadJson(storageKey, fallback);
