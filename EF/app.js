3983aaa3d8f328292ffbf8304a312ee7f5b40b92
kalish1213-eng <kalish1213@gmail.com>
2026-06-13 16:59:42 +0300

    Upgrade 8B native lesson imagery to lightweight WebP

--- EF/app.js
+++ EF/app.js
@@ -241,7 +241,7 @@ const nativeLessons = [
     startPage: 51,
     endPage: 52,
     title: "8B I love cooking",
-    url: "public/native/8b-i-love-cooking/index.html?v=20260613-8bphoto1"
+    url: "public/native/8b-i-love-cooking/index.html?v=20260613-8bphoto2"
   },
   {
     startPage: 53,
