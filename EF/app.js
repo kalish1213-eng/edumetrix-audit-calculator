fcd272c1464c87188653bf9fbd65f07e4efeb47a
kalish1213-eng <kalish1213@gmail.com>
2026-06-13 16:49:31 +0300

    Upgrade 8A native lesson imagery to lightweight WebP

--- EF/app.js
+++ EF/app.js
@@ -235,7 +235,7 @@ const nativeLessons = [
     startPage: 49,
     endPage: 50,
     title: "8A Can I park here?",
-    url: "public/native/8a-can-i-park-here/index.html?v=20260613-8aphoto1"
+    url: "public/native/8a-can-i-park-here/index.html?v=20260613-8aphoto2"
   },
   {
     startPage: 51,
