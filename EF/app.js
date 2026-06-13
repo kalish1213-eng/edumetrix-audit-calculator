0190dffbe0be8e1984157fdfa267acc82873d332
kalish1213-eng <kalish1213@gmail.com>
2026-06-13 05:50:56 +0300

    Upgrade 3B native lesson imagery

--- EF/app.js
+++ EF/app.js
@@ -151,7 +151,7 @@ const nativeLessons = [
     startPage: 21,
     endPage: 22,
     title: "3B Souvenirs",
-    url: "public/native/3b-souvenirs/index.html?v=20260612-native3b5"
+    url: "public/native/3b-souvenirs/index.html?v=20260613-3bphoto1"
   },
   {
     startPage: 23,
