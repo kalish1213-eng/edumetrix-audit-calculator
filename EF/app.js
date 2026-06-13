5bf90eb79d15cf0fa69ea845e73569e21ed25047
kalish1213-eng <kalish1213@gmail.com>
2026-06-13 08:25:32 +0300

    Upgrade Revise and Check 5-6 imagery

--- EF/app.js
+++ EF/app.js
@@ -211,7 +211,7 @@ const nativeLessons = [
     startPage: 41,
     endPage: 42,
     title: "Revise and Check 5&6",
-    url: "public/native/revise-check-5-6/index.html?v=20260612-revise56d"
+    url: "public/native/revise-check-5-6/index.html?v=20260613-revise56photo1"
   },
   {
     startPage: 43,
