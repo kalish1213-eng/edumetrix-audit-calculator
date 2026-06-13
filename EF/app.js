aa4dfecf22f7e32f41393057616b2822744e7b30
kalish1213-eng <kalish1213@gmail.com>
2026-06-13 05:06:27 +0300

    Upgrade 2B native lesson imagery

--- EF/app.js
+++ EF/app.js
@@ -133,7 +133,7 @@ const nativeLessons = [
     startPage: 15,
     endPage: 16,
     title: "2B That's my bus!",
-    url: "public/native/2b-thats-my-bus/index.html?v=20260612-native2b"
+    url: "public/native/2b-thats-my-bus/index.html?v=20260613-2bphoto1"
   },
   {
     startPage: 17,
