0dd8ecc88ea27bff9411e164102685f2fef5fd3c
kalish1213-eng <kalish1213@gmail.com>
2026-06-14 03:52:55 +0300

    Improve 3A task imagery

--- EF/src/state/useAnswersState.js
+++ EF/src/state/useAnswersState.js
@@ -1,4 +1,4 @@
-import { loadJson, saveJson } from "../utils/storage.js?v=20260614-navsingle1";
+import { loadJson, saveJson } from "../utils/storage.js?v=20260614-3aphotoitems1";
 
 export function createAnswersState(storageKey, fallback = {}) {
   let value = loadJson(storageKey, fallback);
--- EF/src/state/useWorkbookState.js
+++ EF/src/state/useWorkbookState.js
@@ -1,4 +1,4 @@
-import { getStartPageFromHash } from "../utils/pageNavigation.js?v=20260614-navsingle1";
+import { getStartPageFromHash } from "../utils/pageNavigation.js?v=20260614-3aphotoitems1";
 
 export function createWorkbookState({ hash = location.hash, pageCount = 137 } = {}) {
   return {
