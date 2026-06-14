cfce8b5a6e4755dcd375b8c949558014215cacb6
kalish1213-eng <kalish1213@gmail.com>
2026-06-15 00:36:42 +0300

    Improve Communication exercise photo cards

--- EF/src/data/lessons.js
+++ EF/src/data/lessons.js
@@ -289,7 +289,7 @@ export const nativeLessons = [
     startPage: 79,
     endPage: 86,
     title: "Communication",
-    url: "public/native/communication/index.html?v=20260614-commphoto5"
+    url: "public/native/communication/index.html?v=20260614-commphoto6"
   },
   {
     startPage: 87,
