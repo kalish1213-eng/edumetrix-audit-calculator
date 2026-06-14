14526cba3d2a116c2a10395811d6ca60a01b9488
kalish1213-eng <kalish1213@gmail.com>
2026-06-14 18:59:20 +0300

    Replace PE6 map illustrations with photo assets

--- EF/src/data/lessons.js
+++ EF/src/data/lessons.js
@@ -265,7 +265,7 @@ export const nativeLessons = [
     startPage: 71,
     endPage: 72,
     title: "Practical English 6",
-    url: "public/native/practical-english-6/index.html?v=20260614-pe6map1"
+    url: "public/native/practical-english-6/index.html?v=20260614-pe6photo1"
   },
   {
     startPage: 73,
