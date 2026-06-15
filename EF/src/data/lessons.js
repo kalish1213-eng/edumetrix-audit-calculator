083fd705a562c52928b6129c699e53fcdd42b002
kalish1213-eng <kalish1213@gmail.com>
2026-06-15 04:09:54 +0300

    Refresh communication photo assets

--- EF/src/data/lessons.js
+++ EF/src/data/lessons.js
@@ -289,7 +289,7 @@ export const nativeLessons = [
     startPage: 79,
     endPage: 86,
     title: "Communication",
-    url: "public/native/communication/index.html?v=20260615-commphoto7"
+    url: "public/native/communication/index.html?v=20260615-commphoto8"
   },
   {
     startPage: 87,
